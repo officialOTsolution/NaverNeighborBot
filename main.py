c47a9d6ae64d3d0992a80133ab4bbc73b6b6c248
aigenpysics <godgodgod96@naver.com>
2023-10-22 21:16:09 +0900

    코드 가시성 높힘

--- main.py
+++ main.py
@@ -15,14 +15,12 @@ import LoginUi
 환경: python 3.9.16 my_proj:conda
 """
 app = QApplication(sys.argv)
-
 form_class = uic.loadUiType("LoginUi.ui")[0]
 form_class_1 = uic.loadUiType("MainUi.ui")[0]
 
 class MyWindow(QMainWindow, LoginUi.Ui_Dialog):
     progress_start = pyqtSignal(int)
     progress_finish = pyqtSignal()
-
     def __init__(self):
         super().__init__()
         self.selen = None
@@ -30,7 +28,6 @@ class MyWindow(QMainWindow, LoginUi.Ui_Dialog):
         self.StartBtn.clicked.connect(self.resume)
         self.HowToUse.clicked.connect(self.manul_link)
         self.InstaLInk.clicked.connect(self.insta_link)
-
     def resume(self):
         LoginInfor = self.Login()
         if LoginInfor == 1:
@@ -40,10 +37,8 @@ class MyWindow(QMainWindow, LoginUi.Ui_Dialog):
             self.selen.show()
         else:
             self.show_alert('로그인 실패 \n아이디와 패스워드를 다시 입력해주세요.')
-
     def pause(self):
         self.selen.pause()
-
     def Login(self):
         if self.ID.text() == 'admin' and self.PW.text() == "1004":
             return 1
@@ -56,11 +51,9 @@ class MyWindow(QMainWindow, LoginUi.Ui_Dialog):
         alert.setIcon(QMessageBox.Information)
         alert.setStandardButtons(QMessageBox.Ok)
         alert.exec_()
-
     def insta_link(self):
         url = r"https://www.instagram.com/onetouch_sol/" 
         webbrowser.open(url)
-    
     def manul_link(self):
         url = r"https://cafe.naver.com/onetouchsolution" 
         webbrowser.open(url)
@@ -76,11 +69,11 @@ class SecondWindow(QMainWindow, form_class_1):
         self.message = self.message.text()
         self.IdCollectBtn.clicked.connect(self.StartCollect)
         self.AddFriendBtn.clicked.connect(self.NaverLog)
-        self.worker_thread = None
 
     def StartCollect(self):
         try:
             print("SecondWindow->StartCollec 함수 호출\n")
+            self.flag = True 
             print(self.KeyWordList, self)
             time.sleep(1)
             self.KeyWordList= self.KeyWord.text()
@@ -92,21 +85,32 @@ class SecondWindow(QMainWindow, form_class_1):
             print("SecondWindow->MacroCollect.start() 함수 종료\n")
         except Exception as e:
             print("SecondWindow->StartCollec 함수 에러: "+e)
+    
+    def NaverLog(self):
+        if self.flag:
+            if self.Id.text() != "" and self.Pw.text() != "":
+                NaverLogin = NaverLoginClass(self.driver,self.Id.text(), self.Pw.text())
+                NaverLoginReturn = NaverLogin.run()
+                if NaverLoginReturn == 1:
+                    self.startFriendAdd()
+            else:
+                self.show_alert("네이버 아이디를 먼저 입력해주세요.")
+        else:
+            self.show_alert("아이디 수집을 먼저 진행해주세요.")
+
     def startFriendAdd(self):
         self.friend_thread = FriendAddClass(self.driver, self.MacroCollect.IdList)
         self.friend_thread.update_signal.connect(self.update_gui)
         self.friend_thread.start()
     def update_gui(self, message):
         self.CollectStatus2.append(message)
-    def NaverLog(self):
-        NaverLogin = NaverLoginClass(self.driver,self.Id.text(), self.Pw.text())
-        NaverLoginReturn = NaverLogin.run()
-        if NaverLoginReturn == 1:
-            self.startFriendAdd()
-
-    # def FriendAdd(self):
-    #     FriendMacro = FriendAddClass(self.driver, self.MacroCollect.IdList,  self.CollectStatus2)
-    #     FriendMacro.run()
+    def show_alert(self, text):
+        alert = QMessageBox()
+        alert.setWindowTitle("알림")
+        alert.setText(text)
+        alert.setIcon(QMessageBox.Information)
+        alert.setStandardButtons(QMessageBox.Ok)
+        alert.exec_()
         
 if __name__ == "__main__":
     myWindow = MyWindow()
